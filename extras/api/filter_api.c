792cd60890d3e37f593f17239d7ae1fcce6ee0db
Charles Longeau <github@chl.be>
2014-06-20 01:50:44 +0200

    opensmtpd-extras is now portable. Some cleanups and fixes still need to be done

--- extras/api/filter_api.c
+++ extras/api/filter_api.c
@@ -17,6 +17,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "includes.h"
+
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/uio.h>
@@ -982,7 +984,7 @@ filter_api_sockaddr_to_text(const struct sockaddr *sa)
 {
 	static char	buf[NI_MAXHOST];
 
-	if (getnameinfo(sa, sa->sa_len, buf, sizeof(buf), NULL, 0,
+	if (getnameinfo(sa, SA_LEN(sa), buf, sizeof(buf), NULL, 0,
 		NI_NUMERICHOST))
 		return ("(unknown)");
 	else
