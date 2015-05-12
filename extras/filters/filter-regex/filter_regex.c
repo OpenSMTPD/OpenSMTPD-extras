1f0051b9412b61a42d162fc200eb649c576fe498
Joerg Jung <mail@umaxx.net>
2015-05-12 23:35:28 +0200

    tweak code and bring back -l limit option and '!' operator

--- extras/filters/filter-regex/filter_regex.c
+++ extras/filters/filter-regex/filter_regex.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h>
 #include <regex.h>
 #include <unistd.h>
 #include <util.h>
@@ -38,56 +39,64 @@
 #define REGEX_CONF "/etc/mail/filter-regex.conf"
 
 struct regex {
-	SIMPLEQ_ENTRY(regex) entries;
-	regex_t pattern;
+	SIMPLEQ_ENTRY(regex) el;
+	char *s, n;
+	regex_t p;
 };
 
-static SIMPLEQ_HEAD(, regex) re_connect = SIMPLEQ_HEAD_INITIALIZER(re_connect);
-static SIMPLEQ_HEAD(, regex) re_helo = SIMPLEQ_HEAD_INITIALIZER(re_helo);
-static SIMPLEQ_HEAD(, regex) re_mail = SIMPLEQ_HEAD_INITIALIZER(re_mail);
-static SIMPLEQ_HEAD(, regex) re_rcpt = SIMPLEQ_HEAD_INITIALIZER(re_rcpt);
-static SIMPLEQ_HEAD(, regex) re_dataline = SIMPLEQ_HEAD_INITIALIZER(re_dataline);
+static SIMPLEQ_HEAD(regex_q, regex)
+	regex_connect = SIMPLEQ_HEAD_INITIALIZER(regex_connect),
+	regex_helo = SIMPLEQ_HEAD_INITIALIZER(regex_helo),
+	regex_mail = SIMPLEQ_HEAD_INITIALIZER(regex_mail),
+	regex_rcpt = SIMPLEQ_HEAD_INITIALIZER(regex_rcpt),
+	regex_dataline = SIMPLEQ_HEAD_INITIALIZER(regex_dataline);
+static struct { const char *s; struct regex_q *rq; } regex_s[] = {
+	{ "connect", &regex_connect }, { "helo", &regex_helo },
+	{ "mail", &regex_mail }, { "rcpt", &regex_rcpt },
+	{ "dataline", &regex_dataline }, { NULL, NULL } };
+static unsigned int regex_limit;
+
+static char *
+regex_skip(char *s) {
+	while (isspace((unsigned char)*s))
+		s++;
+	return s;
+}
 
 static int
 regex_parse(char *l, size_t no)
 {
+	struct regex_q *rq = NULL;
 	struct regex *re;
 	char *k, buf[BUFSIZ];
-	int r;
+	int i, r;
 
-	while (isspace((unsigned char)*l))
-		l++;
-	if (strlen(l) == 0)
+	l = regex_skip(l);
+	if ((k = strsep(&l, " \t")) == NULL || strlen(k) == 0)
 		return 0; /* skip empty line */
-	k = strsep(&l, " \t");
-	while (isspace((unsigned char)*l))
-		l++;
-	if (strlen(l) == 0) {
-		log_warnx("warn: filter-regex: parse: missing regex (line: %lu)", no);
+	for (i = 0; regex_s[i].s != NULL && rq == NULL; i++)
+		if (strcmp(k, regex_s[i].s) == 0)
+			rq = regex_s[i].rq;
+	if (rq == NULL) {
+		log_warnx("warn: filter-regex: parse: unknown keyword %s line %lu", k, no);
+		return -1;
+	}
+	if (strlen((l = regex_skip(l))) == 0) {
+		log_warnx("warn: filter-regex: parse: missing value line %lu", no);
 		return -1;
 	}
 	re = xcalloc(1, sizeof(struct regex), "filter-regex: parse");
-	if ((r = regcomp(&re->pattern, l, REG_EXTENDED|REG_NOSUB)) != 0) {
-		regerror(r, &re->pattern, buf, sizeof(buf));
-		log_warnx("warn: filter-regex: parse: regcomp %s (line: %lu)", buf, no);
-		free(re);
+	re->s = xstrdup(l, "filter-regex: parse");
+	if ((re->n = (l[0] == '!' && isspace((unsigned char)l[1]))))
+		l = regex_skip(++l);
+	if ((r = regcomp(&re->p, l, REG_EXTENDED|REG_NOSUB)) != 0) {
+		regerror(r, &re->p, buf, sizeof(buf));
+		log_warnx("warn: filter-regex: parse: regcomp %s line %lu", buf, no);
+		free(re->s), free(re);
 		return -1;
 	}
-	if (strcmp(k, "connect") == 0)
-		SIMPLEQ_INSERT_TAIL(&re_connect, re, entries);
-	else if (strcmp(k, "helo") == 0)
-		SIMPLEQ_INSERT_TAIL(&re_helo, re, entries);
-	else if (strcmp(k, "mail") == 0)
-		SIMPLEQ_INSERT_TAIL(&re_mail, re, entries);
-	else if (strcmp(k, "rcpt") == 0)
-		SIMPLEQ_INSERT_TAIL(&re_rcpt, re, entries);
-	else if (strcmp(k, "dataline") == 0)
-		SIMPLEQ_INSERT_TAIL(&re_dataline, re, entries);
-	else {
-		log_warnx("warn: filter-regex: parse: unknown regex keyword %s (line: %lu)", k, no);
-		free(re);
-		return -1;
-	}
+	SIMPLEQ_INSERT_TAIL(rq, re, el);
+	log_debug("debug: filter-regex: parse: %s %s line %lu", k, re->s, no);
 	return 0;
 }
 
@@ -96,7 +105,7 @@ regex_load(const char *c)
 {
 	FILE *f;
 	char *l;
-	size_t no;
+	size_t no = 0;
 
 	if ((f = fopen(c, "r")) == NULL) {
 		log_warn("warn: filter-regex: load: fopen %s", c);
@@ -104,8 +113,7 @@ regex_load(const char *c)
 	}
 	while ((l = fparseln(f, NULL, &no, NULL, 0)) != NULL) {
 		if (regex_parse(l, no) == -1) {
-			free(l);
-			fclose(f);
+			free(l), fclose(f);
 			return -1;
 		}
 		free(l);
@@ -115,56 +123,46 @@ regex_load(const char *c)
 }
 
 static int
-regex_match(regex_t *pattern, const char *s)
+regex_match(struct regex_q *rq, const char *s)
 {
-	int r = regexec(pattern, s, 0, NULL, 0);
+	struct regex *re;
 	char buf[BUFSIZ];
+	int r;
 
-	if (r != 0 && r != REG_NOMATCH) {
-		regerror(r, pattern, buf, sizeof(buf));
-		log_warnx("warn: filter-regex: match: regexec %s", buf);
-		return 0;
+	SIMPLEQ_FOREACH(re, rq, el) {
+		if ((r = regexec(&re->p, s, 0, NULL, 0)) != 0) {
+			if (r != REG_NOMATCH) {
+				regerror(r, &re->p, buf, sizeof(buf));
+				log_warnx("warn: filter-regex: match: regexec %s", buf);
+			}
+			continue;
+		}
+		log_info("info: filter-regex: match: %s to %s", re->s, s);
+		return (re->n == 0);
 	}
-	return (r == 0);
+	return 0;
 }
 
 static void
 regex_clear(void)
 {
 	struct regex *re;
+	int i;
 
-	while((re = SIMPLEQ_FIRST(&re_connect)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&re_connect, entries);
-		regfree(&re->pattern), free(re);
-	}
-	while((re = SIMPLEQ_FIRST(&re_helo)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&re_helo, entries);
-		regfree(&re->pattern), free(re);
-	}
-	while((re = SIMPLEQ_FIRST(&re_mail)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&re_mail, entries);
-		regfree(&re->pattern), free(re);
-	}
-	while((re = SIMPLEQ_FIRST(&re_rcpt)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&re_rcpt, entries);
-		regfree(&re->pattern), free(re);
-	}
-	while((re = SIMPLEQ_FIRST(&re_dataline)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&re_dataline, entries);
-		regfree(&re->pattern), free(re);
+	for (i = 0; regex_s[i].rq != NULL; i++) {
+		while((re = SIMPLEQ_FIRST(regex_s[i].rq)) != NULL) {
+			SIMPLEQ_REMOVE_HEAD(regex_s[i].rq, el);
+			regfree(&re->p), free(re->s), free(re);
+		}
 	}
 }
 
 static int
 regex_on_connect(uint64_t id, struct filter_connect *c)
 {
-	struct regex *re;
-
-	SIMPLEQ_FOREACH(re, &re_connect, entries) {
-		if (regex_match(&re->pattern, c->hostname)) {
-			log_warnx("filter-regex: on_connect: REJECT connect hostname id=%016"PRIx64, id);
-			return filter_api_reject_code(id, FILTER_FAIL, 554, "Hostname rejected"); /* todo: better code/message? */
-		}
+	if (regex_match(&regex_connect, c->hostname)) {
+		log_warnx("filter-regex: on_connect: REJECT connect hostname id=%016"PRIx64, id);
+		return filter_api_reject_code(id, FILTER_FAIL, 554, "Hostname rejected");
 	}
 	return filter_api_accept(id);
 }
@@ -172,13 +170,9 @@ regex_on_connect(uint64_t id, struct filter_connect *c)
 static int
 regex_on_helo(uint64_t id, const char *h)
 {
-	struct regex *re;
-
-	SIMPLEQ_FOREACH(re, &re_helo, entries) {
-		if (regex_match(&re->pattern, h)) {
-			log_warnx("filter-regex: on_helo: REJECT helo hostname id=%016"PRIx64, id);
-			return filter_api_reject_code(id, FILTER_FAIL, 554, "Helo rejected"); /* todo: better code/message? */
-		}
+	if (regex_match(&regex_helo, h)) {
+		log_warnx("filter-regex: on_helo: REJECT helo hostname id=%016"PRIx64, id);
+		return filter_api_reject_code(id, FILTER_FAIL, 554, "Helo rejected");
 	}
 	return filter_api_accept(id);
 }
@@ -186,14 +180,9 @@ regex_on_helo(uint64_t id, const char *h)
 static int
 regex_on_mail(uint64_t id, struct mailaddr *m)
 {
-	struct regex *re;
-	const char *s = filter_api_mailaddr_to_text(m);
-
-	SIMPLEQ_FOREACH(re, &re_mail, entries) {
-		if (regex_match(&re->pattern, s)) {
-			log_warnx("filter-regex: on_mail: REJECT mail from id=%016"PRIx64, id);
-			return filter_api_reject_code(id, FILTER_FAIL, 554, "Sender rejected"); /* todo: better code/message? */
-		}
+	if (regex_match(&regex_mail, filter_api_mailaddr_to_text(m))) {
+		log_warnx("filter-regex: on_mail: REJECT mail from id=%016"PRIx64, id);
+		return filter_api_reject_code(id, FILTER_FAIL, 554, "Sender rejected");
 	}
 	return filter_api_accept(id);
 }
@@ -201,55 +190,39 @@ regex_on_mail(uint64_t id, struct mailaddr *m)
 static int
 regex_on_rcpt(uint64_t id, struct mailaddr *r)
 {
-	struct regex *re;
-	const char *s = filter_api_mailaddr_to_text(r);
-
-	SIMPLEQ_FOREACH(re, &re_rcpt, entries) {
-		if (regex_match(&re->pattern, s)) {
-			log_warnx("filter-regex: on_rcpt: REJECT rcpt to id=%016"PRIx64, id);
-			return filter_api_reject_code(id, FILTER_FAIL, 554, "Recipient rejected"); /* todo: better code/message? */
-		}
+	if (regex_match(&regex_rcpt, filter_api_mailaddr_to_text(r))) {
+		log_warnx("filter-regex: on_rcpt: REJECT rcpt to id=%016"PRIx64, id);
+		return filter_api_reject_code(id, FILTER_FAIL, 554, "Recipient rejected");
 	}
-	return filter_api_accept(id);
-}
-
-static int
-regex_on_data(uint64_t id)
-{
-	int *m = xcalloc(1, sizeof(int), "filter-regex: on_data");
-
-	filter_api_set_udata(id, m);
 	return filter_api_accept(id);
 }
 
 static void
 regex_on_dataline(uint64_t id, const char *l)
 {
-	int *m;
-	struct regex *re;
+	struct { int m; unsigned int l; } *u;
 
 	filter_api_writeln(id, l);
-	if ((m = filter_api_get_udata(id)) == NULL || *m)
-		return;
-	SIMPLEQ_FOREACH(re, &re_dataline, entries) {
-		if (regex_match(&re->pattern, l)) {
-			*m = 1;
-			break;
-		}
+	if ((u = filter_api_get_udata(id)) == NULL) {
+		u = xcalloc(1, sizeof(*u), "filter-regex: on_dataline");
+		filter_api_set_udata(id, u);
 	}
+	if (u->m || (regex_limit && ++u->l >= regex_limit))
+		return;
+	u->m = regex_match(&regex_dataline, l);
 }
 
 static int
 regex_on_eom(uint64_t id, size_t size)
 {
-	int *m, r;
+	int r, *m;
 
 	if ((m = filter_api_get_udata(id)) == NULL)
 		return filter_api_accept(id);
 	r = *m, free(m), filter_api_set_udata(id, NULL);
 	if (r) {
 		log_warnx("filter-regex: on_eom: REJECT dataline id=%016"PRIx64, id);
-		return filter_api_reject_code(id, FILTER_CLOSE, 554, "Message content rejected"); /* todo: better code/message? */
+		return filter_api_reject_code(id, FILTER_CLOSE, 554, "Message content rejected");
 	}
 	return filter_api_accept(id);
 }
@@ -279,11 +252,15 @@ int
 main(int argc, char **argv)
 {
 	int	ch;
+	const char *errstr, *l = NULL;
 
 	log_init(-1);
 
-	while ((ch = getopt(argc, argv, "")) != -1) {
+	while ((ch = getopt(argc, argv, "l:")) != -1) {
 		switch (ch) {
+		case 'l':
+			l = optarg;
+			break;
 		default:
 			log_warnx("warn: filter-regex: bad option");
 			return (1);
@@ -295,6 +272,12 @@ main(int argc, char **argv)
 	if (argc > 1)
 		fatalx("filter-regex: bogus argument(s)");
 
+	if (l) {
+		regex_limit = strtonum(l, 1, UINT_MAX, &errstr);
+		if (errstr)
+			fatalx("filter-regex: limit option is %s: %s", errstr, l);
+	}
+
 	log_debug("debug: filter-regex: starting...");
 	if (regex_load((argc == 1) ? argv[0] : REGEX_CONF) == -1)
 		fatalx("filter-regex: configuration failed");
@@ -303,7 +286,6 @@ main(int argc, char **argv)
 	filter_api_on_helo(regex_on_helo);
 	filter_api_on_mail(regex_on_mail);
 	filter_api_on_rcpt(regex_on_rcpt);
-	filter_api_on_data(regex_on_data);
 	filter_api_on_dataline(regex_on_dataline);
 	filter_api_on_eom(regex_on_eom);
 	filter_api_on_reset(regex_on_reset);
