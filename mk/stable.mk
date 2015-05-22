3590d9631601aed5901dffacdd2521e5439e2243
Gilles Chehade <gilles@poolp.org>
2015-05-22 03:47:38 +0200

    move all filters to wip/ and update build to use proper include

---
+++ mk/stable.mk
@@ -0,0 +1,15 @@
+api_srcdir		= $(top_srcdir)/api
+asr_srcdir		= $(top_srcdir)/contrib/lib/libc/asr
+compat_srcdir		= $(top_srcdir)/openbsd-compat
+regress_srcdir		= $(top_srcdir)/regress/bin
+
+filters_srcdir		= $(top_srcdir)/extras/stable/filters
+queues_srcdir		= $(top_srcdir)/extras/stable/queues
+schedulers_srcdir	= $(top_srcdir)/extras/stable/schedulers
+tables_srcdir		= $(top_srcdir)/extras/stable/tables
+
+PATHS=		-DSMTPD_CONFDIR=\"$(sysconfdir)\"			\
+		-DPATH_CHROOT=\"$(PRIVSEP_PATH)\"			\
+		-DPATH_SMTPCTL=\"$(sbindir)/smtpctl\"			\
+		-DPATH_MAILLOCAL=\"$(pkglibexecdir)/mail.local\"	\
+		-DPATH_LIBEXEC=\"$(pkglibexecdir)\"
--- mk/wip.mk
+++ mk/wip.mk
@@ -1,11 +1,12 @@
 api_srcdir		= $(top_srcdir)/api
-filters_srcdir		= $(top_srcdir)/extras/filters
-queues_srcdir		= $(top_srcdir)/extras/queues
-schedulers_srcdir	= $(top_srcdir)/extras/schedulers
-tables_srcdir		= $(top_srcdir)/extras/tables
 asr_srcdir		= $(top_srcdir)/contrib/lib/libc/asr
 compat_srcdir		= $(top_srcdir)/openbsd-compat
 regress_srcdir		= $(top_srcdir)/regress/bin
+
+filters_srcdir		= $(top_srcdir)/extras/wip/filters
+queues_srcdir		= $(top_srcdir)/extras/wip/queues
+schedulers_srcdir	= $(top_srcdir)/extras/wip/schedulers
+tables_srcdir		= $(top_srcdir)/extras/wip/tables
 
 PATHS=		-DSMTPD_CONFDIR=\"$(sysconfdir)\"			\
 		-DPATH_CHROOT=\"$(PRIVSEP_PATH)\"			\
