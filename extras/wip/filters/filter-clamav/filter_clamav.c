58c213a972129c20abdec30a70468fc1fbd38396
Joerg Jung <mail@umaxx.net>
2016-06-20 23:26:36 +0200

    convert clamav filter to non-blocking io

--- extras/wip/filters/filter-clamav/filter_clamav.c
+++ extras/wip/filters/filter-clamav/filter_clamav.c
@@ -29,103 +29,50 @@
 #include "smtpd-api.h"
 #include "log.h"
 #include "iobuf.h"
+#include "ioev.h"
+
+#define CLAMAV_HOST "127.0.0.1"
+#define CLAMAV_PORT "3310"
 
 struct clamav {
-	int fd, r;
+	uint64_t id;
 	struct iobuf iobuf;
+	struct io io;
+	int r;
+	enum { CL_DATA, CL_EOM, CL_STA } s;
 };
 
-static const char *clamav_host = "127.0.0.1", *clamav_port = "3310";
+static struct sockaddr_storage clamav_ss;
 
 static void
-clamav_init(struct clamav *cl)
+clamav_clear(struct clamav *cl)
 {
-	cl->fd = cl->r = -1;
-	iobuf_xinit(&cl->iobuf, LINE_MAX, LINE_MAX, "init");
+	if (cl == NULL)
+		return;
+	io_clear(&cl->io);
+	iobuf_clear(&cl->iobuf);
+	free(cl);
 }
 
 static int
-clamav_open(struct clamav *cl)
+clamav_result(struct clamav *cl)
 {
-	struct addrinfo hints, *addresses, *ai;
-	int r;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = PF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV; /* avoid failing name resolution in chroot() */
-	if ((r = getaddrinfo(clamav_host, clamav_port, &hints, &addresses))) {
-		log_warnx("warn: open: getaddrinfo %s", gai_strerror(r));
+	if (cl->r == INT_MIN) {
+		log_warnx("warn: result: failed");
 		return -1;
 	}
-	for (ai = addresses; ai; ai = ai->ai_next) {
-		if ((cl->fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) == -1)
-			continue;
-		if (connect(cl->fd, ai->ai_addr, ai->ai_addrlen) == -1) {
-			close(cl->fd);
-			cl->fd = -1;
-			continue;
-		}
-		break; /* connected */
+	if (cl->r) {
+		log_warnx("warn: session %016"PRIx64": result: REJECT virus", cl->id);
+		return filter_api_reject_code(cl->id, FILTER_CLOSE, 554, "5.7.1 Virus found");
 	}
-	freeaddrinfo(addresses);
-	if (!ai) {
-		log_warnx("warn: open: failed");
-		return -1;
-	}
-	return 0;
-}
-
-static int
-clamav_write(struct clamav *cl, const char *l, int f) {
-	size_t len = (f == EOF) ? 0 : strlen(l) + 1;
-	uint32_t n = htonl(len);
-
-	if (f && iobuf_queue(&cl->iobuf, &n, sizeof(uint32_t)) != (int)sizeof(uint32_t)) {
-		log_warn("warn: write: iobuf_queue");
-		return -1;
-	}
-	if (f != EOF && iobuf_fqueue(&cl->iobuf, "%s\n", l) != (int)len) {
-		log_warn("warn: write: iobuf_fqueue");
-		return -1;
-	}
-	if (iobuf_flush(&cl->iobuf, cl->fd) < 0) {
-		log_warn("warn: write: iobuf_flush");
-		return -1;
-	}
-	return 0;
-}
-
-static int
-clamav_request(struct clamav *cl) {
-	return clamav_write(cl, "nINSTREAM", 0);
-}
-
-static int
-clamav_read(struct clamav *cl, char **l) {
-	int r;
-
-	while ((*l = iobuf_getline(&cl->iobuf, NULL)) == NULL) {
-		if (iobuf_len(&cl->iobuf) >= LINE_MAX) {
-			log_warnx("warn: read: iobuf_getline");
-			return -1;
-		}
-		iobuf_normalize(&cl->iobuf);
-		if ((r = iobuf_read(&cl->iobuf, cl->fd)) < 0) {
-			if (r != IOBUF_CLOSED)
-				log_warn("warn: read: iobuf_read r=%d", r);
-			return r;
-		}
-	}
-	return 0;
+	return filter_api_accept(cl->id);
 }
 
 #define CLAMAV_EXPAND(tok) #tok
 #define CLAMAV_QUOTE(tok) CLAMAV_EXPAND(tok)
 
 static int
-clamav_result(struct clamav *cl, const char *l) {
+clamav_status(struct clamav *cl, const char *l) {
 	char s[BUFSIZ + 1];
 
 	if (sscanf(l, "stream: %"CLAMAV_QUOTE(BUFSIZ)"s", s) != 1) {
@@ -138,49 +85,83 @@ clamav_result(struct clamav *cl, const char *l) {
 }
 
 static int
-clamav_message(struct clamav *cl) {
-	char *l = NULL;
-
-	if (clamav_read(cl, &l) != 0)
-		return -1;
-	if (clamav_result(cl, l) == -1)
-		return -1;
-	if (cl->r == -1) {
-		log_warnx("warn: message: result failed");
-		return -1;
-	}
-	if (iobuf_len(&cl->iobuf)) {
-		log_warnx("warn: message: incomplete");
-		return -1;
+clamav_response(struct clamav *cl, const char *l)
+{
+	switch (cl->s) {
+	case CL_STA:
+		if (clamav_status(cl, l) == -1)
+			return -1;
+		break;
+	default:
+		fatalx("response: bad state");
 	}
 	return 0;
 }
 
-static int
-clamav_response(struct clamav *cl) {
-	if (clamav_write(cl, "", EOF))
-		return -1;
-	if (clamav_message(cl) == -1)
-		return -1;
-	return 0;
+static void
+clamav_io(struct io *io, int evt)
+{
+	struct clamav *cl = io->arg;
+	char *l;
+	uint32_t n = htonl(0);
+
+	switch (evt) {
+	case IO_CONNECTED:
+		io_set_write(io);
+		break;
+	case IO_LOWAT:
+		if (cl->s == CL_EOM) {
+			if (iobuf_queue(&cl->iobuf, &n, sizeof(uint32_t)) != (int)sizeof(uint32_t)) {
+				log_warn("warn: io: iobuf_queue");
+				goto fail;
+			}
+			cl->s++;
+			io_set_read(io);
+		}
+		break;
+	case IO_DATAIN:
+		while ((l = iobuf_getline(&cl->iobuf, NULL))) {
+			if (iobuf_len(&cl->iobuf) >= LINE_MAX) {
+				log_warnx("warn: io: iobuf_getline");
+				goto fail;
+			}
+			if (clamav_response(cl, l) == -1)
+				goto fail;
+		}
+		iobuf_normalize(&cl->iobuf);
+		break;
+	case IO_DISCONNECTED:
+		if (cl->s == CL_STA) {
+			if (iobuf_len(&cl->iobuf)) {
+				log_warnx("warn: io: incomplete");
+				goto fail;
+			}
+			if (clamav_result(cl) == -1)
+				goto fail;
+			io_clear(io);
+			break;
+		} /* FALLTHROUGH */
+	case IO_TIMEOUT:
+	case IO_ERROR:
+		log_warnx("warn: io: %s %s", io_strevent(evt), cl->io.error);
+		goto fail;
+	default:
+		fatalx("io: bad event");
+	}
+	return;
+fail:
+	filter_api_reject_code(cl->id, FILTER_FAIL, 451, "4.7.1 Virus filter failed");
+	filter_api_set_udata(cl->id, NULL);
+	clamav_clear(cl);
 }
 
 static void
-clamav_close(struct clamav *cl)
+clamav_init(struct clamav *cl, uint64_t id)
 {
-	if (cl->fd >= 0)
-		close(cl->fd);
-	cl->fd = -1;
-}
-
-static void
-clamav_clear(struct clamav *cl)
-{
-	if (cl == NULL)
-		return;
-	iobuf_clear(&cl->iobuf);
-	clamav_close(cl);
-	free(cl);
+	iobuf_xinit(&cl->iobuf, LINE_MAX, LINE_MAX, "init");
+	io_init(&cl->io, -1, cl, clamav_io, &cl->iobuf);
+	cl->id = id;
+	cl->r = INT_MIN;
 }
 
 static int
@@ -188,15 +169,13 @@ clamav_on_data(uint64_t id)
 {
 	struct clamav *cl;
 
-	clamav_init((cl = xcalloc(1, sizeof(struct clamav), "on_data")));
-	if (clamav_open(cl) == -1) {
+	clamav_init((cl = xcalloc(1, sizeof(struct clamav), "on_data")), id);
+	if (io_connect(&cl->io, (struct sockaddr *)&clamav_ss, NULL) == -1) {
+		log_warnx("warn: on_data: io_connect %s", cl->io.error);
 		clamav_clear(cl);
 		return filter_api_accept(id);
 	}
-	if (clamav_request(cl) == -1) {
-		clamav_clear(cl);
-		return filter_api_accept(id);
-	}
+	iobuf_xfqueue(&cl->iobuf, "on_data", "nINSTREAM\n");
 	filter_api_set_udata(id, cl);
 	return filter_api_accept(id);
 }
@@ -205,12 +184,16 @@ static void
 clamav_on_dataline(uint64_t id, const char *l)
 {
 	struct clamav *cl;
+	uint32_t n;
 
 	filter_api_writeln(id, l);
 	if ((cl = filter_api_get_udata(id)) == NULL)
 		return;
-	if (cl->fd >= 0 && clamav_write(cl, l, 1) == -1)
-		clamav_close(cl);
+	n = htonl(strlen(l) + 1);
+	if (iobuf_queue(&cl->iobuf, &n, sizeof(uint32_t)) != (int)sizeof(uint32_t))
+		fatalx("on_dataline: iobuf_queue");
+	iobuf_xfqueue(&cl->iobuf, "%s\n", l);
+	io_reload(&cl->io);
 }
 
 static int
@@ -220,16 +203,10 @@ clamav_on_eom(uint64_t id, size_t size)
 
 	if ((cl = filter_api_get_udata(id)) == NULL)
 		return filter_api_accept(id);
-	if (clamav_response(cl) == -1) {
-		clamav_clear(cl);
-		filter_api_set_udata(id, NULL);
-		return filter_api_reject_code(id, FILTER_FAIL, 451, "4.7.1 Virus filter failed");
-	}
-	if (cl->r) {
-		log_warnx("warn: session %016"PRIx64": on_eom: REJECT virus", id);
-		return filter_api_reject_code(id, FILTER_CLOSE, 554, "5.7.1 Virus found");
-	}
-	return filter_api_accept(id);
+	cl->s++;
+	if (iobuf_queued(&cl->iobuf) == 0)
+		clamav_io(&cl->io, IO_LOWAT);
+	return 1; /* defer accept or reject */
 }
 
 static void
@@ -246,22 +223,46 @@ clamav_on_rollback(uint64_t id)
 	filter_api_set_udata(id, NULL);
 }
 
+
+static void
+clamav_resolve(const char *h, const char *p)
+{
+	struct addrinfo hints, *addresses, *ai;
+	int fd, r;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = PF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	if ((r = getaddrinfo(h, p, &hints, &addresses)))
+		fatalx("resolve: getaddrinfo %s", gai_strerror(r));
+	for (ai = addresses; ai; ai = ai->ai_next) {
+		if ((fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) == -1)
+			continue;
+		if (connect(fd, ai->ai_addr, ai->ai_addrlen) == -1) {
+			close(fd);
+			continue;
+		}
+		write(fd, "nPING\n", 6);
+		close(fd);
+		memmove(&clamav_ss, ai->ai_addr, ai->ai_addrlen);
+		break;
+	}
+	freeaddrinfo(addresses);
+	if (!ai)
+		fatalx("resolve: failed");
+}
+
 int
 main(int argc, char **argv)
 {
-	int ch, C = 0, d = 0, v = 0;
-	char *c = NULL, *h = NULL, *p = NULL;
+	int ch, d = 0, v = 0;
+	char *h = CLAMAV_HOST, *p = CLAMAV_PORT;
 
 	log_init(1);
 
-	while ((ch = getopt(argc, argv, "Cc:dh:p:v")) != -1) {
+	while ((ch = getopt(argc, argv, "dh:p:v")) != -1) {
 		switch (ch) {
-		case 'C':
-			C = 1;
-			break;
-		case 'c':
-			c = optarg;
-			break;
 		case 'd':
 			d = 1;
 			break;
@@ -283,27 +284,22 @@ main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (c)
-		c = strip(c);
 	if (h)
-		clamav_host = strip(h);
+		h = strip(h);
 	if (p)
-		clamav_port = strip(p);
+		p = strip(p);
 
 	log_init(d);
 	log_verbose(v);
 
 	log_debug("debug: starting...");
+	clamav_resolve(h, p);
 
 	filter_api_on_data(clamav_on_data);
 	filter_api_on_dataline(clamav_on_dataline);
 	filter_api_on_eom(clamav_on_eom);
 	filter_api_on_commit(clamav_on_commit);
 	filter_api_on_rollback(clamav_on_rollback);
-	if (c)
-		filter_api_set_chroot(c);
-	if (C)
-		filter_api_no_chroot();
 
 	filter_api_loop();
 	log_debug("debug: exiting");
